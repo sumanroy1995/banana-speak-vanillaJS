cf6a1362cf8cb59da4bf9ad1619ff45e3d871f85
sumanroy1995 <sumanroy8909@gmail.com>
2023-07-20 15:54:04 +0530

    taking input in console

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var button= document.querySelector("#btn-click");
-function clickEventHandler(){
+var button = document.querySelector("#btn-click");
+var textInput=document.querySelector("#txt-input");
+
+function clickEventHandler() {
     console.log("Clicked!");
+    console.log(textInput.value);
 };
 button.addEventListener("click", clickEventHandler); //callback to the function clickEventHandler
-
-
