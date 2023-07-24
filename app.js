12f9842fbabacf2fa9e4675f9c543aa2caf59074
sumanroy1995 <sumanroy8909@gmail.com>
2023-07-24 14:18:59 +0530

    output is ready

--- app.js
+++ app.js
@@ -3,18 +3,25 @@ var textInput = document.querySelector("#txt-input");
 var output = document.querySelector("#output");
 
 function getTranslated(text) {
-     return "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text="+text
+     return "https://api.funtranslations.com/translate/navi.json?text="+text; //the api url taken from funtranslations.com
+}
+
+function errorHandler(error){
+     console.log("Server Error"); 
+     alert("Server is not reachable!");
 }
 
 function clickEventHandler() {
-     // output.innerText = "Translated: " + textInput.value;
      var txtVal= textInput.value;
+     console.log(getTranslated(txtVal));
      fetch(getTranslated(txtVal)) //the fetch api is calling to fetch or request the URL's data from the server 
      .then(function responseHandler(response){ //then convering the response into a JSON file by using then()
           return response.json()
      })
      .then(function getJSON(json){//then to show the log of that JSON file by using then()
-          console.log(json)
+          output.innerText= json.contents.translated;
+          console.log(json.contents.translated)
      })
-}
+     .catch(errorHandler) //we don't need to call errorHandler() because the fetch api library is providing the "error" argument inside the catch()
+};
 button.addEventListener("click", clickEventHandler); //callback to the function clickEventHandler
